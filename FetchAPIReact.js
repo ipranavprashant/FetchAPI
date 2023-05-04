2ce7c297e63a70d19d5f60fddb625c857f54578e
Pranav Prashant <97225953+ipranavprashant@users.noreply.github.com>
2023-05-04 16:49:26 +0530

    Update FetchAPIReact.js

--- FetchAPIReact.js
+++ FetchAPIReact.js
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from 'react'
 const FetchAPIReact =() => {
     const [convertedJson,setConvertedJson]=useState([]);
     const handleFetch=async()=>{
+        try{
         const fetchedData=await fetch('https://api.github.com/users');
     setConvertedJson(await fetchedData.json());
     console.log(convertedJson);
+        }catch(error){
+            console.log("Error hai bhai!!"+error);
+        }
     }
     
     useEffect(()=>{
